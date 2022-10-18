8acea9060fe04fc51733a762109e0c59d015cdf5
Yhossy Esteban Navas Peñaranda <yhossyenp@algartech.com>
2022-10-18 16:53:47 -0500

    Se adiciona persistencia a una BBDD , y se realiza todo CRUD

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,15 @@ async function bootstrap() {
   //anteponer en el URL de los endpoints EJ api
   app.setGlobalPrefix('api/v2');
   //validacion de los request Body
+  //  -se utiliza transform para que en el request tomen el tipo del valor que se esta enviando EJ: limit === Number
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
     }),
   );
   await app.listen(3000);
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,22 +1,36 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  private readonly axios: AxiosInstance = axios;
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
+  ) {}
 
   async executeSeed() {
-    const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=10',
+    //borramos TODOS los registros
+    await this.pokemonModel.deleteMany({});
+    //Array de insercion
+    const pokemonToInsert: { name: string; no: number }[] = [];
+    const data = await this.http.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
 
-    data.results.forEach(({ name, url }) => {
-      //https://pokeapi.co/api/v2/pokemon/1/
+    data.results.forEach(async ({ name, url }) => {
+      // url = https://pokeapi.co/api/v2/pokemon/1/
       const segments = url.split('/');
       const no: number = +segments[segments.length - 2];
+      pokemonToInsert.push({ no, name });
     });
+    //insertamos los registros
+    await this.pokemonModel.insertMany(pokemonToInsert);
 
-    return data.results;
+    return 'Seed Executed';
   }
 }
