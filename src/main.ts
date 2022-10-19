dce4502800233760fe2497b89328f5f9bc9542cb
Yhossy Esteban Navas Peñaranda <yhossyenp@algartech.com>
2022-10-19 14:33:06 -0500

    Add swagger in the proyect

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -18,6 +19,16 @@ async function bootstrap() {
       },
     }),
   );
+  //swagger
+  const config = new DocumentBuilder()
+    .setTitle('Pokemons example')
+    .setDescription('The Pokemons API')
+    .setVersion('1.0')
+    .addTag('pokemons')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,11 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsInt, IsPositive, IsString, Min, MinLength } from 'class-validator';
 
 export class CreatePokemonDto {
   @IsInt()
   @IsPositive()
   @Min(1)
+  @ApiProperty({
+    type: Number,
+    minimum: 1,
+  })
   no: number;
   @IsString()
   @MinLength(1)
+  @ApiProperty({
+    type: String,
+    minLength: 1,
+  })
   name: string;
 }
